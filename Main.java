3453b7426ca12eaf59ab9dd769caafd7ca8bd87e
Nilesh Gupta <NileshGupta@Nileshs-MacBook-Pro.local>
2016-03-14 17:01:50 -0500

    Initial code

---
+++ Main.java
@@ -0,0 +1,146 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Main {
+	
+	Node node;
+	int totalNodes;
+	int interRequestDelay;
+	int csExecutionTime;
+	int numberOfRequest;
+	
+	public Main()
+	{
+		node = new Node();
+	}
+
+	public static void main(String[] args) {
+	
+		
+		
+		int nodeNumber = Integer.parseInt(args[0]);
+		File f = new File(args[1]);
+		
+		Main m = new Main();
+		m.node.setId(nodeNumber);
+		m.readConfigFile(nodeNumber,f);
+		
+		SocketConnectionServer server = new SocketConnectionServer(m.node);
+		server.start();
+		
+		
+		
+
+	}
+	
+	public void readConfigFile(int nodeNumber, File f)
+	{
+		FileReader fileReader;
+		try {
+			fileReader = new FileReader(f);
+			BufferedReader br = new BufferedReader(fileReader);
+			ArrayList<Node> aln = new ArrayList<Node>();
+			
+			String line1 = br.readLine();
+			String[] words = line1.split("\t",-1);
+			for(int i=0;i<words.length;i++)
+			{
+				words[i] = words[i].trim();
+			}
+			totalNodes = Integer.parseInt(words[0]);
+			interRequestDelay = Integer.parseInt(words[1]);
+			csExecutionTime = Integer.parseInt(words[2]);
+			numberOfRequest = Integer.parseInt(words[3]);
+						
+			for(int i=0;i<totalNodes;i++)
+			{
+				String line2= br.readLine();
+				
+				String[] hostNameLine = line2.split("\t",-1);
+				for(int j=0;j<hostNameLine.length;j++)
+				{
+				
+					hostNameLine[j]=hostNameLine[j].trim();
+				}
+				
+				
+				Node n = new Node();
+				n.setId(Integer.parseInt(hostNameLine[0]));
+				n.setHostname(hostNameLine[1]);
+				n.setPortNumber(Integer.parseInt(hostNameLine[2]));
+				
+				
+				
+				aln.add(n);
+			}
+			
+			
+			HashMap<Integer,ArrayList<Node>> hm = new HashMap<Integer,ArrayList<Node>>();
+	
+			for(int i=0;i<totalNodes;i++)
+			{
+				ArrayList<Node> quorum = new ArrayList<Node>();
+				String line2= br.readLine();
+				
+				String[] childLine = line2.split("\t",-1);
+				for(int j=0;j<childLine.length;j++)
+				{
+					childLine[j]=childLine[j].trim();
+					if(childLine[j].equalsIgnoreCase(""))
+						childLine[j] = "a";
+					else
+						childLine[j] = childLine[j];
+				}
+				for(int j=0;j<childLine.length;j++)
+				{
+					if(!childLine[j].equalsIgnoreCase("a"))
+					{				
+						Node n = new Node();
+						n.setId(Integer.parseInt(childLine[j]));
+						
+						n.setHostname(aln.get(n.getId()).getHostname());
+						n.setPortNumber(aln.get(n.getId()).getPortNumber());
+						
+						quorum.add(n);
+					}
+				}
+				
+				hm.put(i, quorum);
+			}
+			
+			node.setHostname(aln.get(nodeNumber).getHostname());
+			node.setPortNumber(aln.get(nodeNumber).getPortNumber());
+			node.setQuorum(hm.get(nodeNumber));
+			Node parent = new Node();
+			parent.setId(9999);
+			
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public Node getNode() {
+		return node;
+	}
+
+	public void setNode(Node node) {
+		this.node = node;
+	}
+	
+	
+
+}
---
+++ Node.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+
+
+public class Node {
+
+	int id;
+	int portNumber;
+	String hostname;
+	ArrayList<Node> quorum;
+	ArrayList<Node> queue;
+	Node grantOwner;
+	boolean failedReceived;
+	boolean grantFlag;
+	boolean inquireFlag;
+	int timestamp =0;
+	ArrayList<Node> grant;
+	ArrayList<Node> inquire;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPortNumber() {
+		return portNumber;
+	}
+	public void setPortNumber(int portNumber) {
+		this.portNumber = portNumber;
+	}
+	
+	public String getHostname() {
+		return hostname;
+	}
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+	public ArrayList<Node> getQuorum() {
+		return quorum;
+	}
+	public void setQuorum(ArrayList<Node> quorum) {
+		this.quorum = quorum;
+	}
+	public ArrayList<Node> getQueue() {
+		return queue;
+	}
+	public void setQueue(ArrayList<Node> queue) {
+		this.queue = queue;
+	}
+	public Node getGrantOwner() {
+		return grantOwner;
+	}
+	public void setGrantOwner(Node grantOwner) {
+		this.grantOwner = grantOwner;
+	}
+	public boolean isFailedReceived() {
+		return failedReceived;
+	}
+	public void setFailedReceived(boolean failedReceived) {
+		this.failedReceived = failedReceived;
+	}
+	public boolean isGrantFlag() {
+		return grantFlag;
+	}
+	public void setGrantFlag(boolean grantFlag) {
+		this.grantFlag = grantFlag;
+	}
+	public boolean isInquireFlag() {
+		return inquireFlag;
+	}
+	public void setInquireFlag(boolean inquireFlag) {
+		this.inquireFlag = inquireFlag;
+	}
+	public int getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(int timestamp) {
+		this.timestamp = timestamp;
+	}
+	public ArrayList<Node> getGrant() {
+		return grant;
+	}
+	public void setGrant(ArrayList<Node> grant) {
+		this.grant = grant;
+	}
+	public ArrayList<Node> getInquire() {
+		return inquire;
+	}
+	public void setInquire(ArrayList<Node> inquire) {
+		this.inquire = inquire;
+	}
+	
+	
+}
