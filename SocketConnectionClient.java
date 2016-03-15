7d8ec2644ab4899cc9be2e51e2480c3312cd49e1
Nilesh Gupta <NileshGupta@Nileshs-MacBook-Pro.local>
2016-03-14 21:21:52 -0500

    client code for single message

--- SocketConnectionClient.java
+++ SocketConnectionClient.java
@@ -5,17 +5,34 @@ import java.util.ArrayList;
 public class SocketConnectionClient extends Thread{
 
 	ArrayList<Message> am;
+	Message m;
 
 	public SocketConnectionClient(ArrayList<Message> am)
 	{
 		this.am = am;
+	}
+	public SocketConnectionClient(Message m)
+	{
+		am = new ArrayList<Message>();
+		this.m = m;
 	}
 
 	public void run()
 	{
 		try 
 		{
-			for(Message m : am)
+			if(am.size()>0)
+			{				
+				for(Message m1 : am)
+				{
+					Socket client = new Socket(m1.getDestinationNode().getHostname(), m1.getDestinationNode().getPortNumber());
+					ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream());
+					oos.writeObject(m1);
+					oos.close();
+					client.close();
+				}
+			}
+			else
 			{
 				Socket client = new Socket(m.getDestinationNode().getHostname(), m.getDestinationNode().getPortNumber());
 				ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream());
